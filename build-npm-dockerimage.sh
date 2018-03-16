2aab80bd83dfc827b5ece16706dec22a97ba59c5
Andy Riley <Andyianriley@gmail.com>
2018-03-16 07:34:22 +0000

    remove image publish

--- build-npm-dockerimage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-docker build -t aws_lambda_node_6103 .
-docker tag aws_lambda_node_6103:latest eu.gcr.io/k8-discovery-185615/aws_lambda_node_6103:latest
-docker push eu.gcr.io/k8-discovery-185615/aws_lambda_node_6103:latest
